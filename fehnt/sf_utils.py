d81e298aab68a00e2e1ca89e0af1696eac990156
CrepeGoat <awqatty.b@gmail.com>
2019-05-07 21:47:03 -0400

    Renamed `pd_sf_convert.py` to `sf_utils` - added new methods to `HashableSeries` to replace `sub` & `mul` methods from `pandas`     - perform manual index expansion to match indices of other operand TODO test methods

--- fehnt/sf_utils.py
+++ fehnt/sf_utils.py
@@ -10,13 +10,37 @@ class HashableSeries(sf.Series):
 
         return hash(tuple(items))
 
-
     def __eq__(self, other):
         return (
             len(self) == len(other)
             and (self.values == other.values).all()
             and (self.index.values == other.index.values).all()
         )
+
+    def match_index_to(self, target, fill_value=None):
+        # Designed to replace calls like `s1.add(s2, fill_value=0)`
+        if isinstance(target, sf.Series):
+            target_index = target.index
+        else:
+            target_index = target
+
+        return self.__class__((
+            self[i] if i in self else fill_value
+            for i in target_index
+        ), index=target_index)
+
+    def broadcast_index_to(self, target, level_in_target):
+        # Designed to replace calls like `s1.add(s2, level=0)`
+        if isinstance(target, sf.Series):
+            target_index = target.index
+        else:
+            target_index = target
+
+        return self.__class__((
+            self[i[level_in_target]] for i in target_index
+        ), index=target_index)
+
+
 
 def pd_ify(ntuple):
     return ntuple._replace(**{k: v.to_pandas()
