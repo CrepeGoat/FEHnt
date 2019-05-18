6774fcac25a947017b7c43bc67c3dff0b1069db2
CrepeGoat <awqatty.b@gmail.com>
2019-05-17 22:50:40 -0400

    Added test for `branch_event` - note: test raises warning re: ordering `Colors` objects

--- tests/test_core.py
+++ tests/test_core.py
@@ -11,14 +11,15 @@ from fehnt.core import *
 @pytest.fixture
 def pool_probs():
     value = pd.DataFrame.from_records([
-        ('5', 'red', 3/32),
-        ('5', 'blue', 4/32),
-        ('5', 'green', 4/32),
+        (StarPools._5_STAR, Colors.RED, 2/24),
+        (StarPools._5_STAR, Colors.BLUE, 3/24),
+        (StarPools._5_STAR, Colors.GREEN, 3/24),
 
-        ('4', 'red', 7/32),
-        ('4', 'blue', 7/32),
-        ('4', 'green', 7/32),
+        (StarPools._4_STAR, Colors.RED, 6/24),
+        (StarPools._4_STAR, Colors.BLUE, 5/24),
+        (StarPools._4_STAR, Colors.GREEN, 5/24),
     ], columns=['star', 'color', 'probability'])
+    assert value['probability'].sum() == 1
     # value['probability'] = sp.symbols('p0:6', nonnegative=True)
     return value.set_index(['star', 'color'])['probability']
 
@@ -26,13 +27,13 @@ def pool_probs():
 @pytest.fixture
 def pool_counts():
     value = pd.DataFrame.from_records([
-        ('5', 'red', 2),
-        ('5', 'blue', 2),
-        ('5', 'green', 2),
+        (StarPools._5_STAR, Colors.RED, 2),
+        (StarPools._5_STAR, Colors.BLUE, 2),
+        (StarPools._5_STAR, Colors.GREEN, 2),
 
-        ('4', 'red', 4),
-        ('4', 'blue', 4),
-        ('4', 'green', 4),
+        (StarPools._4_STAR, Colors.RED, 4),
+        (StarPools._4_STAR, Colors.BLUE, 4),
+        (StarPools._4_STAR, Colors.GREEN, 4),
     ], columns=['star', 'color', 'count'])
     # value['count'] = sp.symbols('c0:6', nonnegative=True, integer=True)
     return value.set_index(['star', 'color'])['count']
@@ -41,7 +42,7 @@ def pool_counts():
 @pytest.fixture
 def target_pool_counts():
     value = pd.DataFrame.from_records([
-        ('5', 'red', 1),
+        (StarPools._5_STAR, Colors.RED, 1),
     ], columns=['star', 'color', 'count'])
     # value['count'] = sp.symbols('c0:1', nonnegative=True, integer=True)
     return value.set_index(['star', 'color'])['count']
@@ -79,6 +80,34 @@ def test_OutcomeCalculator_init_new_session(monkeypatch):
     assert len(outcome_calc.states) == len(expt_subprobs)
     for state, prob in outcome_calc.states.items():
         assert prob in expt_subprobs
+
+
+def test_OutcomeCalculator_branch_event(monkeypatch, pool_probs, pool_counts,
+                                        target_pool_counts):
+    # Stubs
+    outcome_calc = mock.Mock(spec=OutcomeCalculator,
+                             event_details=mock.Mock(pool_counts=pool_counts),
+                             summoner=mock.Mock(targets=target_pool_counts),
+                             states=defaultdict(int))
+    outcome_calc.event_details.pool_probs.return_value = pool_probs
+    outcome_calc.event_details.pool_counts.return_value = pool_counts
+    event = EventState(orb_count=sp.symbols('orb_count'),
+                       dry_streak=sp.symbols('dry_streak'),
+                       targets_pulled=0*target_pool_counts)
+    session = SessionState(prob_level=1, stone_counts=pd.Series(
+        [1, 0, 0], index=[Colors.RED, Colors.BLUE, Colors.GREEN]
+    ))
+    # probability = sp.symbols('p', nonnegative=True)
+    probability = 1/2
+    stone_choice = Colors.RED
+
+    # Run test
+    OutcomeCalculator.branch_event(outcome_calc, event, session, probability,
+                                   stone_choice)
+
+    probs = (1/16, 1/16, 3/8)
+    assert len(outcome_calc.states) == len(probs)
+    assert sorted(p for p in outcome_calc.states.values()) == sorted(probs)
 
 
 if __name__ == '__main__':
