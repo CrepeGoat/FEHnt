4e4d1009873e599646ad20d6305862b9ab0e2559
CrepeGoat <awqatty.b@gmail.com>
2020-05-23 16:10:39 -0400

    refactored probability-variables' names

--- fehnt/core.py
+++ fehnt/core.py
@@ -75,25 +75,25 @@ class OutcomeCalculator:
         )
         self.branch_event(event, new_session, prob, stone_choice)
 
-    def init_new_session(self, event, probability):
+    def init_new_session(self, event, prob):
         """Add new summoning session after an existing session finishes."""
         if not self.summoner.should_start_new_session(event.targets_pulled):
             self.callback('quit summoning event')
-            self.push_outcome(event, probability)
+            self.push_outcome(event, prob)
             return
 
         self.states[StateStruct(event, SessionState(
-            prob_level=event.dry_streak // SUMMONS_PER_SESSION,
+            prob_tier=event.dry_streak // SUMMONS_PER_SESSION,
             stone_summons=sf.Series(0, index=tuple(Colors)),
             stone_presences=sf.Series(True, index=tuple(Colors)),
-        ))] += probability
+        ))] += prob
 
     def branch_event(self, event, session, prob, stone_choice):
         """Split session into all potential following sub-sessions."""
         orb_count = event.orb_count - stone_cost(session.stone_summons.sum()-1)
 
         choice_starpool_probs = (self.event_details
-                                 .pool_probs(session.prob_level)
+                                 .pool_probs(session.prob_tier)
                                  [sf.HLoc[:, stone_choice]]
                                  .reindex_drop_level(-1))
 
@@ -132,10 +132,10 @@ class OutcomeCalculator:
                     total_prob * subsubprob
                 )
 
-    def push_outcome(self, event, probability):
+    def push_outcome(self, event, prob):
         """Add a given probabilistic outcome to the recorded results."""
         result = ResultState(event.orb_count, event.targets_pulled)
-        self.outcomes[result] += probability
+        self.outcomes[result] += prob
 
     def __call__(self, no_of_orbs):
         """Calculate the summoning probabilities."""
@@ -170,7 +170,7 @@ class OutcomeCalculator:
                 event.targets_pulled,
                 session.stone_summons.sum(),
                 unit_probs=self.event_details.pool_probs(
-                    probability_tier=session.prob_level
+                    prob_tier=session.prob_tier
                 ) / self.event_details.pool_counts,
             )
             self.process_stone_choice_sequence(
