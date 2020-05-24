2b7cae6ca06dbd5339fb9878fe1a4492de9a8500
CrepeGoat <awqatty.b@gmail.com>
2020-05-24 01:07:16 -0400

    customized plots.py to plot mvp graph of results

--- fehnt/plots.py
+++ fehnt/plots.py
@@ -3,23 +3,25 @@ Reference: matplotlib.org/3.2.1/gallery/lines_bars_and_markers/
            horizontal_barchart_distribution.html
 """
 
+from itertools import takewhile
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-category_names = ['Strongly disagree', 'Disagree',
-                  'Neither agree nor disagree', 'Agree', 'Strongly agree']
-results = {
-    'Question 1': [10, 15, 17, 32, 26],
-    'Question 2': [26, 22, 29, 10, 13],
-    'Question 3': [35, 37, 7, 2, 19],
-    'Question 4': [32, 11, 9, 15, 33],
-    'Question 5': [21, 29, 5, 5, 40],
-    'Question 6': [8, 19, 5, 30, 38]
-}
+def count_pulls(outcomes):
+    """Aggregate probabilities of equal-quantity outcomes."""
+    results = []
+    for state, prob in outcomes.items():
+        pull_count = state.targets_pulled.sum()
+        results.extend(0 for _ in range(pull_count - len(results) + 1))
+
+        results[pull_count] += prob
+
+    return [float(i) for i in results]
 
 
-def survey(results, category_names):
+def survey(outcomes_iter, max_orbs=100):
     """
     Parameters
     ----------
@@ -30,22 +32,35 @@ def survey(results, category_names):
     category_names : list of str
         The category labels.
     """
+    results = {
+        orbs: count_pulls(outcomes)
+        for (orbs, outcomes) in takewhile(
+            lambda x: x[0] <= max_orbs,
+            outcomes_iter,
+        )
+    }
+
+    bin_count = max(len(pull_counts) for pull_counts in results.values())
+    for pull_counts in results.values():
+        pull_counts.extend(0 for _ in range(bin_count - len(pull_counts)))
+
     labels = list(results.keys())
     data = np.array(list(results.values()))
     data_cum = data.cumsum(axis=1)
     category_colors = plt.get_cmap('RdYlGn')(
         np.linspace(0.15, 0.85, data.shape[1]))
 
+    print('time to make the plot')
     fig, ax = plt.subplots(figsize=(9.2, 5))
     ax.invert_yaxis()
     ax.xaxis.set_visible(False)
     ax.set_xlim(0, np.sum(data, axis=1).max())
 
-    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+    for i, color in enumerate(category_colors):
         widths = data[:, i]
         starts = data_cum[:, i] - widths
         ax.barh(labels, widths, left=starts, height=0.5,
-                label=colname, color=color)
+                label=str(i), color=color)
         xcenters = starts + widths / 2
 
         r, g, b, _ = color
@@ -53,11 +68,46 @@ def survey(results, category_names):
         for y, (x, c) in enumerate(zip(xcenters, widths)):
             ax.text(x, y, str(int(c)), ha='center', va='center',
                     color=text_color)
-    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1),
+    ax.legend(ncol=bin_count, bbox_to_anchor=(0, 1),
               loc='lower left', fontsize='small')
 
+    print('plot made!')
     return fig, ax
 
 
-survey(results, category_names)
-plt.show()
+if __name__ == '__main__':
+    from fehnt.core import *
+
+    pool_counts = make_pool_counts(
+        (StarPools.x5_STAR_FOCUS, Colors.RED, 1),
+        (StarPools.x5_STAR_FOCUS, Colors.BLUE, 1),
+        (StarPools.x5_STAR_FOCUS, Colors.GREEN, 1),
+        (StarPools.x5_STAR_FOCUS, Colors.GRAY, 1),
+
+        (StarPools.x5_STAR, Colors.RED, 19),
+        (StarPools.x5_STAR, Colors.BLUE, 15),
+        (StarPools.x5_STAR, Colors.GREEN, 15),
+        (StarPools.x5_STAR, Colors.GRAY, 8),
+
+        (StarPools.x4_STAR, Colors.RED, 32),
+        (StarPools.x4_STAR, Colors.BLUE, 30),
+        (StarPools.x4_STAR, Colors.GREEN, 20),
+        (StarPools.x4_STAR, Colors.GRAY, 28),
+
+        (StarPools.x3_STAR, Colors.RED, 32),
+        (StarPools.x3_STAR, Colors.BLUE, 29),
+        (StarPools.x3_STAR, Colors.GREEN, 19),
+        (StarPools.x3_STAR, Colors.GRAY, 28),
+    )
+
+    target_pool_counts = make_pool_counts(
+        (StarPools.x5_STAR_FOCUS, Colors.RED, 1),
+    )
+
+    outcomes_iter = OutcomeCalculator(
+        event_details=EventDetails.make_standard(pool_counts),
+        summoner=ColorHuntSummoner(target_pool_counts),
+    )
+
+    survey(outcomes_iter, max_orbs=20)
+    plt.show()
